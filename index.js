42f76338f63caa1a27ac3168bd854f4414e04d6a
Kartik Narwal <kartiknarwal38@gmail.com>
2025-11-02 23:47:04 +0530

    nothing chnanged

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.use(express.json());
 // }));
 
 app.use(cors({
-    origin: ["https://ecommerce-frontend-six-vert-45.vercel.app"],
+    origin: ["hhttp://localhost:5173"],
     methods: ["GET","POST","PUT","DELETE","OPTIONS"],
     credentials: true
 }));
